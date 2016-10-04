220d855c103a2c6243aa6892bc110c3ade00a08f
Waxhoya <waxhoya@gmail.com>
2016-10-04 12:56:11 -0700

    The weather API calls dynamically replaces text below the main logo depending on the weather. Additionally the weather is displayed in the upper right corner in abs positioning

--- scripts/model/weather.js
+++ scripts/model/weather.js
@@ -2,15 +2,25 @@ var weatherAPI = {};
 
 $.get('http://api.wunderground.com/api/f25e47528444be94/conditions/q/OR/Portland.json')
 	.done(function(data){
-	console.log(data)
+	console.log(data);
 	weatherAPI = data;
 	$(Weather).text(
-		"PDX currently: "+
-		weatherAPI.current_observation.weather + " Temp: "+
-		weatherAPI.current_observation.temp_c + " Celcius, and Rain is: " +
+		"PDX: "+
+		weatherAPI.current_observation.weather + " "+
+		weatherAPI.current_observation.temp_c + "C. Rain: " +
 		weatherAPI.current_observation.precip_today_string + "today."
-	)
+	);
+		if (parseInt(weatherAPI.current_observation.temp_f) > 65) {
+			$(tempPlaceholder).text(" Hot");
+		};
+		if (parseInt(weatherAPI.current_observation.UV) > 4) {
+			$(lightPlaceholder).text("Bright");
+		};
+		if (parseInt(weatherAPI.current_observation.precip_today_metric) > 0) {
+			$(moistPlaceholder).text(" and wet...");
+		};
 	});
+
 
 
 	console.log('Temp Celcius: ', weatherAPI.current_observation.temp_c) // returns temp in C
