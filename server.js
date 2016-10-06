086ac65890ab0fcc26a9ed758abaa0d2cb273888
Waxhoya <waxhoya@gmail.com>
2016-10-05 20:24:02 -0700

    added express proxy

---
+++ server.js
@@ -0,0 +1,6 @@
+
+// include express proxy for API requests
+var express = require('express');
+var requestProxy = require('express-request-proxy');
+var car port = process.env.PORT || 3000,
+var app = express();
