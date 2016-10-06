36807db648a07040d656196a19405214bb7e3b08
Waxhoya <waxhoya@gmail.com>
2016-10-05 20:46:57 -0700

    cleaned up server.js

--- server.js
+++ server.js
@@ -2,5 +2,16 @@
 // include express proxy for API requests
 var express = require('express');
 var requestProxy = require('express-request-proxy');
-var car port = process.env.PORT || 3000,
+var port = process.env.PORT || 3000;
 var app = express();
+
+function getGit(request, response) {(
+  requestProxy({
+    url: 'https://api.github.com/' + reqest.params[0],
+    headers: {
+      Authorization: 'token ' + process.env.GIT_TOKEN
+    }
+  })(request, response)
+);};
+
+app.get('/github/*', getGit);
